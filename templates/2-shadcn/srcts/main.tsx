cb264c6631854e3bac5776b9c17c24de091b856d
Winston Chang <winston@posit.co>
2025-09-11 22:33:43 -0500

    Move file to correct location

--- templates/2-shadcn/srcts/main.tsx
+++ templates/2-shadcn/srcts/main.tsx
@@ -1,5 +1,5 @@
-import { App } from "@/components/App";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 import "./globals.css";
 
 const container = document.getElementById("root");
